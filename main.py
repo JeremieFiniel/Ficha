19bca40a3f26abd3a36297c558f0bac01919ad02
jeremiefiniel <jeremie.finiel@laposte.net>
2019-08-11 17:07:27 +0200

    remove keyboard gpio

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from grove_lcd import *
+#!/usr/bin/python3
 import RPi.GPIO as GPIO
 import subprocess
 import time
@@ -12,86 +12,64 @@ server = socket.socket()
 server_host = "192.168.1.50"
 server_port = 8123
 
+def setText(text):
+    print(text);
+
+
 def capture():
-	global cameraRecorde
-	if cameraRecorde:
-		print("Camera allready recorde")
-	else:
-		cameraRecorde = True
+    global cameraRecorde
+    if cameraRecorde:
+        print("Camera allready recorde")
+    else:
+        cameraRecorde = True
 
-		print("Start motor")
-		GPIO.output(relay, GPIO.HIGH)
+        print("Start motor")
+        GPIO.output(relay, GPIO.HIGH)
 
-		date = dt.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+        date = dt.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
 
-		camera = picamera.PiCamera()
-		print("Recorde start")
-		setText('Recording video')
+        camera = picamera.PiCamera()
+        print("Recorde start")
+        setText('Recording video')
 
-		camera.start_recording(date+'.h264')
-		camera.wait_recording(10)
+        camera.start_recording(date+'.h264')
+        camera.wait_recording(10)
 
-		print("Stop motor")
-		GPIO.output(relay, GPIO.LOW)
+        print("Stop motor")
+        GPIO.output(relay, GPIO.LOW)
 
-		setText('Stop recording')
-		camera.stop_recording()
-		setText('Identifiez vous')
-		print("Recorde stop")
-		camera.close()
-		cameraRecorde = False
+        setText('Stop recording')
+        camera.stop_recording()
+        setText('Identifiez vous')
+        print("Recorde stop")
+        camera.close()
+        cameraRecorde = False
 
-		print("Rename video file")
-		command = "MP4Box -add {}.h264 {}.mp4".format(date, date)
-		try:
-		    output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
-		except subprocess.CalledProcessError as e:
-			print('FAIL:\ncmd:{}\noutput:{}'.format(e.cmd, e.output))
+        print("Rename video file")
+        command = "MP4Box -add {}.h264 {}.mp4".format(date, date)
+        try:
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as e:
+            print('FAIL:\ncmd:{}\noutput:{}'.format(e.cmd, e.output))
 
-		print('File renamed')
+        print('File renamed')
 
-		print('Remove old file')
-		os.remove(date+'.h264')
+        print('Remove old file')
+        os.remove(date+'.h264')
 
-		print('Send video to server')
-		server.connect((server_host, server_port))
-		server.send(date+'.mp4')
-		with open(date+'.mp4', 'rb') as file:
-			chunk = file.read(1024*8)
-			while chunk:
-				server.send(chunk)
-				chunk = file.read(1024*8)
+        print('Send video to server')
+        server.connect((server_host, server_port))
+        server.send(date+'.mp4')
+        with open(date+'.mp4', 'rb') as file:
+            chunk = file.read(1024*8)
+            while chunk:
+                server.send(chunk)
+                chunk = file.read(1024*8)
 
-		print "done sending"
-		server.close()
-
-
-
-def buttonPressedCB(button):
-	time.sleep(.1)
-	if GPIO.input(button) :
-		setText ("Button %s pressed"%buttons[button]);
-		print('Button pressed %s is %s'%(button, buttons[button]))
-		if buttons[button] == "OK":
-			thread = Thread(target=capture)
-			thread.start()
+        print("done sending")
+        server.close()
 
 
-
-buttons = {
-	4  : "Annule",
-	17 : 1,
-	18 : 7,
-	27 : 0,
-	22 : 8,
-	23 : 4,
-	24 : 5,
-	25 : 2,
-	5 : "OK",
-	6 : 9,
-	13 : 6,
-	19 : 3
-}
 
 relay = 26
 
@@ -99,17 +77,11 @@ GPIO.setmode(GPIO.BCM)
 
 cameraRecorde = False
 
-print('Initialize buttons')
-
-for button, value in buttons.items():
-	GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.add_event_detect(button, GPIO.RISING, callback=buttonPressedCB, bouncetime=200)
-
 GPIO.setup(relay, GPIO.OUT, initial=GPIO.LOW)
 
 setText('Identifiez vous')
 
 while True:
-	raw_input('Press Enter to continue...')
-	thread = Thread(target=capture)
-	thread.start()
+    raw_input('Press Enter to continue...')
+    thread = Thread(target=capture)
+    thread.start()
