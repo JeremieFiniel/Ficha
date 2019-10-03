e732f33986b940b9dba7c3fb210810276ca6d623
jeremiefiniel <jeremie.finiel@laposte.net>
2019-10-03 20:53:58 +0200

    use debug and enum

--- perif/src/main.cpp
+++ perif/src/main.cpp
@@ -11,6 +11,18 @@ char keys[ROWS][COLS] = {
     {'*','0','#'}
 };
 
+enum State {
+    INIT,
+    WAIT,
+};
+
+enum Commande {
+    NO,
+    WRITE,
+    PING
+};
+
+
 byte rowPins[ROWS] = {7, 2, 3, 5}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {6, 8, 4}; //connect to the column pinouts of the keypad
 
@@ -18,13 +30,39 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 LiquidCrystal_I2C lcd(0x27,16,4);
 
+enum Commande commande;
+enum State state;
+
+class Debug {
+    public:
+        Debug(){};
+        void println(String str) {
+            if (enable)
+                Serial.println(str);
+        }
+        void print(String str) {
+            if (enable)
+                Serial.print(str);
+        }
+        bool enable;
+
+};
+
+Debug debug;
+
 void setup() {
     Serial.begin(9600);
     lcd.init();
     lcd.backlight();
     lcd.setCursor(0, 0);
     lcd.print("Coucou");
-    Serial.println("<Startup>");
+    debug.enable = false;
+
+    debug.println("<Startup>");
+
+    commande = NO;
+
+    state = INIT;
 }
 
 void loop() {
@@ -36,13 +74,30 @@ void loop() {
 void serialEvent() {
     while (Serial.available()) {
         char key = Serial.read();
-        if (key == '\n')
-            lcd.setCursor(0,1);
-        else if (key == ';')
-            lcd.clear();
-        else {
-            lcd.print(key);
-            Serial.print(key);
+        switch (commande) {
+            case NO:
+                switch (key) {
+                    case 'w':
+                        commande = WRITE;
+                        debug.println("Go to write mode");
+                        break;
+                    case 'p':
+                        Serial.print("PONG;");
+                        break;
+                }
+                break;
+            case WRITE:
+                if (key == '\n')
+                    lcd.setCursor(0,1);
+                else if (key == ';')
+                    lcd.clear();
+                else if (key == '$')
+                    commande = NO;
+                else {
+                    lcd.print(key);
+                    Serial.print(key);
+                }
+                break;
         }
     }
 }
